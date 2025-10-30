c5d3421137f05d8f49c783c614f3fd4b9543eced
sohagroy7223 <freelancersohagroy@gmail.com>
2025-10-30 12:50:39 +0600

    addad different js

---
+++ different.js
@@ -0,0 +1,10 @@
+// for (let n = 1; n <= 20; n++) {
+//     if (n % 5 === 0 || n % 3 === 0) {
+//         console.log(n);
+//     }
+// }
+for (let n = 1; n <= 30; n++) {
+    if (n % 5 === 0 && n % 3 === 0) {
+        console.log(n);
+    }
+}
